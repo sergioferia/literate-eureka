fb2749a2fc94b0648b5006b52f37101cb9f0dc68
Sergio Feria <sergio@berkeley.edu>
2019-02-02 23:45:31 -0800

    add github user and base services

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,4 +1,6 @@
 class IndexController < ApplicationController
   def index
+    #display rando user
+    @user = Github::User.find("sergioferia")
   end
 end
---
+++ app/services/github/user.rb
@@ -0,0 +1,21 @@
+module Github
+  class User < Base
+    attr_accessor :login,
+                  :avatar_url,
+                  :created_at,
+                  :updated_at,
+                  :id
+    MAX_LIMIT = 12
+    CACHE_DEFAULTS = { expires_in: 7.days, force: false }
+
+    def self.find(id)
+      response = Request.get("users/#{id}", CACHE_DEFAULTS)
+      User.new(response)
+    end
+
+    def initialize(args = {})
+      super(args)
+    end
+
+  end
+end
